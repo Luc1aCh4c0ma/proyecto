59f7bf7e4a0b47d7042a9140f6c2c06a246a9230
Luc1aCh4c0ma <132414863+Luc1aCh4c0ma@users.noreply.github.com>
2024-06-14 20:58:20 -0300

    first

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BreedsModule } from './breeds/breeds.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'proyecto',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CatsModule,
+    BreedsModule,
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
